41e7685dddaf5418d51ff3a1bedab2151e55a080
ErikSaless <eriksales170205@gmail.com>
2026-03-30 08:48:33 -0300

    1commit, agenda_online

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("enter").addEventListener("click", () => {
+    window.location.href = "server/page/page.html";
+})
+
+document.getElementById("recovery").addEventListener("click", () => {
+    window.location.href = "server/recovery_pass/recovery.html";
+})
