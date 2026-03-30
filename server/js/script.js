df2157d94289d363d4e6cbfc420ae6f7f6bef4de
ErikSaless <eriksales170205@gmail.com>
2026-03-30 11:19:37 -0300

    recomeço

--- server/js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("enter").addEventListener("click", () => {
-    window.location.href = "server/page/page.html";
-})
-
-document.getElementById("recovery").addEventListener("click", () => {
-    window.location.href = "server/recovery_pass/recovery.html";
-})
