b5b7f1ecd1080890ae52e4e039a4298f09dfbd53
ErikSaless <eriksales170205@gmail.com>
2026-04-09 18:29:06 -0300

    Atualização

--- server/js/index.js
+++ server/js/index.js
@@ -1,17 +1,13 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-
-
 document.getElementById("enter").addEventListener("click", ()=>{
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
 
-    signinwithEmailAndPassword(auth, user, pass)
-    .then(()=>{
-        window.location.href = "page.html";
-    })
-    .catch((erro)=>{
-        alert("Erro ao logar:  " + erro.message)
-    });
+   if (user === user && pass === pass)
+    {
+        window.location.href="page.html"
+    }
+    else {
+        alert("usuario ou senha invalidos")
+    }
 
 });
